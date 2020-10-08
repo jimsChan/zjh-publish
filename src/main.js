d4c63b5b4e91728867327c57a6f550310fbbbde8
JimsChan <280343091@qq.com>
2020-10-08 15:12:36 +0800

    none

--- src/main.js
+++ src/main.js
@@ -2,6 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// 加载组件库
+import ElementUI from 'element-ui'
+
+// 加载样式
+import 'element-ui/lib/theme-chalk/index.css'
+
+// 自定义样式
+import './styles/index.less'
+
+// 全局注册ele组件库
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 new Vue({
